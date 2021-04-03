068e8f3efe53724325d8b5ca58b3ce6d05728863
Raiki Ali <ali.raiki@student-cs.fr>
2021-04-03 15:09:58 +0200

    Write component to fix test for second scenario of Catalog feature (passing test)

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Catalog({ items }) {
+  if (items.length === 0) return <p>Aucun article n&apos;est disponible</p>;
   return (
     items.map((item) => (
       <div key={item.id} className="item">{item.name}</div>
