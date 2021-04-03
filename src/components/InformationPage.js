7fe3f9db155c3d430594184819be58bedf1c4068
Raiki Ali <ali.raiki@student-cs.fr>
2021-04-03 16:20:17 +0200

    Write component to fix test for second scenario of Info Page feature (passing test)

--- src/components/InformationPage.js
+++ src/components/InformationPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function InformationPage({ item }) {
-  return <p id="description">{item.description}</p>;
+  return <p id="description">{item.description.substring(0, 50)}</p>;
 }
 
 InformationPage.defaultProps = {
