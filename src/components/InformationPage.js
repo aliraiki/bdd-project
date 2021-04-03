8ce8f8569bbf1aacf6aca98fefe1111be41dda2a
Raiki Ali <ali.raiki@student-cs.fr>
2021-04-03 16:10:20 +0200

    Refactor and improve existing code for first scenario of Info App feature

--- src/components/InformationPage.js
+++ src/components/InformationPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function InformationPage({ item }) {
   return <p id="description">{item.description}</p>;
 }
+
+InformationPage.defaultProps = {
+  item: {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+  },
+};
+
+InformationPage.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    price: PropTypes.number,
+  }),
+};
 
 export default InformationPage;
