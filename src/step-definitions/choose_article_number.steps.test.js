428321a682f762ad013fa70499fb9c6d93418057
Raiki Ali <ali.raiki@student-cs.fr>
2021-04-04 10:22:01 +0200

    Write test for first scenario of Article Number feature (failing test)

---
+++ src/step-definitions/choose_article_number.steps.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import { fireEvent, render, screen } from '@testing-library/react';
+import newProduct from '../utils/newProduct';
+import InformationPage from '../components/InformationPage';
+
+const feature = loadFeature('./src/features/choose_article_number.feature');
+
+defineFeature(feature, (test) => {
+  test('Increment number of an item', ({ given, when, then }) => {
+    let informationPage;
+    given(/^a user is on the information page of an item and counter is at (\d+)$/, (articleCount) => {
+      const item = newProduct(1, 'Produit 1');
+      informationPage = render(<InformationPage item={item} />);
+    });
+
+    when('they click on +', () => {
+      const incrementButton = informationPage.getByTestId('increment');
+      fireEvent.click(incrementButton);
+    });
+
+    then(/^the counter should change to (\d+)$/, (newArticleCount) => {
+      const articleCount = informationPage.getByTestId('article-count');
+      expect(articleCount.textContent).toEqual(newArticleCount);
+    });
+  });
+
+  test('Decrement number of an item', ({ given, when, then }) => {
+    given(/^a user is on the information page of an item and counter is at (\d+)$/, (arg0) => {
+
+    });
+
+    when('they click on -', () => {
+
+    });
+
+    then(/^the counter should change to (\d+)$/, (arg0) => {
+      expect(0).toEqual(1);
+    });
+  });
+
+  test('At least one article', ({ given, when, then }) => {
+    given(/^a user is on the information page of an item and counter is at (\d+)$/, (arg0) => {
+
+    });
+
+    when('they click on -', () => {
+
+    });
+
+    then('the counter should not change', () => {
+      expect(0).toEqual(1);
+    });
+  });
+
+  test('No more than 10 articles', ({ given, when, then }) => {
+    given(/^a user is on the information page of an item and counter is at (\d+)$/, (arg0) => {
+
+    });
+
+    when('they click on +', () => {
+
+    });
+
+    then('the counter should not change', () => {
+      expect(0).toEqual(1);
+    });
+  });
+});
