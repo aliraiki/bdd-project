c66cca35cf83e34f121352a4807beb34d71dfd7a
Raiki Ali <ali.raiki@student-cs.fr>
2021-04-04 10:43:50 +0200

    Write test for fourth scenario of Article Number feature (failing test)

--- src/step-definitions/choose_article_number.steps.test.js
+++ src/step-definitions/choose_article_number.steps.test.js
@@ -75,16 +75,27 @@ defineFeature(feature, (test) => {
   });
 
   test('No more than 10 articles', ({ given, when, then }) => {
+    let informationPage;
+    let maximumArticleCount;
     given(/^a user is on the information page of an item and counter is at (\d+)$/, (arg0) => {
-
+      const item = newProduct(1, 'Produit 1');
+      maximumArticleCount = arg0;
+      informationPage = render(<InformationPage item={item} />);
+      const incrementButton = informationPage.getByTestId('increment');
+      // Loop to get the desired initial article number
+      for (let i = 1; i < arg0; i += 1) {
+        fireEvent.click(incrementButton);
+      }
     });
 
     when('they click on +', () => {
-
+      const incrementButton = informationPage.getByTestId('increment');
+      fireEvent.click(incrementButton);
     });
 
     then('the counter should not change', () => {
-      expect(0).toEqual(1);
+      const articleCount = informationPage.getByTestId('article-count');
+      expect(articleCount.textContent).toEqual(maximumArticleCount);
     });
   });
 });
